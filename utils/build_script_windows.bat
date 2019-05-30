5d09b40187ce843e1ca3727999a987a580f17212
sowle <crypto.sowle@gmail.com>
2019-05-30 19:00:00 +0300

    build: build script windows improvements (sha256, more verbose email)

--- utils/build_script_windows.bat
+++ utils/build_script_windows.bat
@@ -14,6 +14,7 @@ IF NOT [%build_prefix%] == [] (
 
 IF NOT [%testnet%] == [] (
   SET TESTNET_DEF=-D TESTNET=TRUE
+  SET TESTNET_LABEL=testnet 
   SET ACHIVE_NAME_PREFIX=%ACHIVE_NAME_PREFIX%testnet-
 )
 
@@ -33,11 +34,6 @@ set BOOST_LIBRARYDIR=%LOCAL_BOOST_LIB_PATH%
 
 
 cd %SOURCES_PATH%
-git pull
-IF %ERRORLEVEL% NEQ 0 (
-  goto error
-)
-
 
 @echo "---------------- BUILDING APPLICATIONS ------------------------"
 @echo "---------------------------------------------------------------"
@@ -54,6 +50,8 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat" x86_amd64
+echo on
+cd %SOURCES_PATH%\build
 
 msbuild version.vcxproj /p:SubSystem="CONSOLE,5.02"  /p:Configuration=Release /t:Build
 IF %ERRORLEVEL% NEQ 0 (
@@ -174,30 +172,34 @@ IF %ERRORLEVEL% NEQ 0 (
 @echo "   UPLOADING TO SERVER ...."
 
 set installer_file=%ACHIVE_NAME_PREFIX%%version%-installer.exe
+set installer_path=%BUILDS_PATH%\builds\%installer_file%
 
-pscp -load zano_build_server %BUILDS_PATH%\builds\%installer_file% build.zano.org:/var/www/html/builds
+pscp -load zano_build_server %installer_path% build.zano.org:/var/www/html/builds
 IF %ERRORLEVEL% NEQ 0 (
   @echo "FAILED TO UPLOAD EXE TO SERVER"
   goto error
 )
+call :sha256 %installer_path% installer_checksum
 
 pscp -load zano_build_server %build_zip_path% build.zano.org:/var/www/html/builds
 IF %ERRORLEVEL% NEQ 0 (
   @echo "FAILED TO UPLOAD ZIP TO SERVER"
   goto error
 )
+call :sha256 %build_zip_path% build_zip_checksum
 
 pscp -load zano_build_server %pdbs_zip_path% build.zano.org:/var/www/html/builds
 IF %ERRORLEVEL% NEQ 0 (
   @echo "FAILED TO UPLOAD PDBS TO SERVER"
   goto error
 )
+call :sha256 %pdbs_zip_path% pdbs_zip_path_checksum
 
-set mail_msg="New build for win-x64 available at http://build.zano.org:8081/builds/%installer_file% <br><br>ZIP:  http://build.zano.org:8081/builds/%build_zip_filename% <br>PDBs: http://build.zano.org:8081/builds/%pdbs_zip_filename%"
+set mail_msg="New %build_prefix% %TESTNET_LABEL%build for win-x64:<br>INST: http://build.zano.org:8081/builds/%installer_file% <br>sha256: %installer_checksum%<br><br>ZIP:  http://build.zano.org:8081/builds/%build_zip_filename% <br>sha256: %build_zip_checksum%<br>PDBs: http://build.zano.org:8081/builds/%pdbs_zip_filename% <br>sha256: %pdbs_zip_path_checksum%"
 
 echo %mail_msg%
 
-senditquiet.exe  -t %emails% -subject "Zano win-x64 build %version%" -body %mail_msg%
+senditquiet.exe  -t %emails% -subject "Zano win-x64 %build_prefix% %TESTNET_LABEL%build %version%" -body %mail_msg%
 
 
 goto success
@@ -211,3 +213,20 @@ echo "BUILD SUCCESS"
 
 cd ..
 
+EXIT /B %ERRORLEVEL%
+
+
+:: functions
+
+:sha256
+@setlocal enabledelayedexpansion
+@set /a count=1 
+@for /f "skip=1 delims=:" %%a in ('CertUtil -hashfile %1 SHA256') do @(
+  @if !count! equ 1 set "hash=%%a"
+  @set /a count+=1
+)
+@(
+ @endlocal
+ @set "%2=%hash: =%
+)
+@exit /B 0
