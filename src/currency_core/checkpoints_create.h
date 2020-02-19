6356b9b09d548914655a064dd2ad64becb3adb5c
sowle <crypto.sowle@gmail.com>
2020-02-19 19:26:40 +0300

    checkpoints for testnet

--- src/currency_core/checkpoints_create.h
+++ src/currency_core/checkpoints_create.h
@@ -9,12 +9,22 @@
 #include "checkpoints.h"
 #include "misc_log_ex.h"
 
-#define ADD_CHECKPOINT(h, hash)  CHECK_AND_ASSERT(checkpoints.add_checkpoint(h,  hash), false);
+#define ADD_CHECKPOINT(h, hash)  CHECK_AND_ASSERT(checkpoints.add_checkpoint(h,  hash), false)
 
-namespace currency {
+namespace currency
+{
+
   inline bool create_checkpoints(currency::checkpoints& checkpoints)
   {
+#ifdef TESTNET
+    ADD_CHECKPOINT(50000,   "cb05a7bdc7f78c5cdb6ef1048f85b27c569f44879233903ce5f5a4e5bd590a3d");
+    ADD_CHECKPOINT(100000,  "6b8b54356a9d44f6c1ebdacb8593d8f5ab2e2e2ca4493e7ae7baf4b3755c5e16");
+#else
+    // MAINNET
+    // no checkpoints
+#endif
 
     return true;
   }
-}
+
+} // namespace currency
