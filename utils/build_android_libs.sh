1cad3e1925a166796452f230f02920f2e58db95a
cryptozoidberg <crypto.zoidberg@gmail.com>
2020-02-28 04:15:50 +0100

    changed android build script to be more friendly to ios builds

--- utils/build_android_libs.sh
+++ utils/build_android_libs.sh
@@ -1,75 +1,75 @@
 set -x #echo on
 cd ..
-rm -r _builds
-cmake -S. -B_builds  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install 
+rm -r _builds_android
+cmake -S. -B_builds_android  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=armeabi-v7a -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_install_android_PREFIX=`pwd`/_install_android 
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
-cmake --build _builds --config Debug --target install -- -j 4
+cmake --build _builds_android --config Debug --target install -- -j 4
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
-rm -r _builds
+rm -r _builds_android
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
-cmake -S. -B_builds  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install 
+cmake -S. -B_builds_android  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_install_android_PREFIX=`pwd`/_install_android 
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
-cmake --build _builds --config Debug --target install -- -j 4
+cmake --build _builds_android --config Debug --target install -- -j 4
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
-rm -r _builds
-if [ $? -ne 0 ]; then
-    echo "Failed to perform command"
-    exit 1
-fi
-
-
-cmake -S. -B_builds  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install 
-if [ $? -ne 0 ]; then
-    echo "Failed to perform command"
-    exit 1
-fi
-
-cmake --build _builds --config Debug --target install -- -j 4
-if [ $? -ne 0 ]; then
-    echo "Failed to perform command"
-    exit 1
-fi
-
-rm -r _builds
+rm -r _builds_android
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
 
-cmake -S. -B_builds  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86_64 -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_INSTALL_PREFIX=`pwd`/_install 
+cmake -S. -B_builds_android  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_install_android_PREFIX=`pwd`/_install_android 
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
-cmake --build _builds --config Debug --target install -- -j 4
+cmake --build _builds_android --config Debug --target install -- -j 4
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
 fi
 
-rm -r _builds
+rm -r _builds_android
+if [ $? -ne 0 ]; then
+    echo "Failed to perform command"
+    exit 1
+fi
+
+
+cmake -S. -B_builds_android  -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 -DCMAKE_ANDROID_ARCH_ABI=x86_64 -DCMAKE_ANDROID_NDK=/Users/roky/Library/Android/sdk/ndk/18.1.5063045 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_install_android_PREFIX=`pwd`/_install_android 
+if [ $? -ne 0 ]; then
+    echo "Failed to perform command"
+    exit 1
+fi
+
+cmake --build _builds_android --config Debug --target install -- -j 4
+if [ $? -ne 0 ]; then
+    echo "Failed to perform command"
+    exit 1
+fi
+
+rm -r _builds_android
 if [ $? -ne 0 ]; then
     echo "Failed to perform command"
     exit 1
