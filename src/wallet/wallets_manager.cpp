9fc930230acb28cb64fb0cb6bf9d197742ee72c6
cryptozoidberg <crypto.zoidberg@gmail.com>
2020-05-24 14:59:55 +0200

    got rid of delay before first update of wallet info

--- src/wallet/wallets_manager.cpp
+++ src/wallet/wallets_manager.cpp
@@ -643,14 +643,15 @@ void wallets_manager::loop()
 
   while (!m_stop_singal_sent)
   {
-    {
-      std::unique_lock<std::mutex> lk(m_stop_singal_sent_mutex);
-      m_stop_singal_sent_mutex_cv.wait_for(lk, std::chrono::microseconds(DAEMON_IDLE_UPDATE_TIME_MS), [&] {return m_stop_singal_sent.load(); });
-    }
     if (!m_stop_singal_sent)
     {
       update_state_info();
     }
+    {
+      std::unique_lock<std::mutex> lk(m_stop_singal_sent_mutex);
+      m_stop_singal_sent_mutex_cv.wait_for(lk, std::chrono::microseconds(DAEMON_IDLE_UPDATE_TIME_MS), [&] {return m_stop_singal_sent.load(); });
+    }
+
 
   }
 //   while(!m_stop_singal_sent)
