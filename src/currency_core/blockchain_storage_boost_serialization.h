6d7096af6edfbba5e7252381e1d30ad9af9fb7cf
sowle <crypto.sowle@gmail.com>
2020-07-07 13:22:05 +0300

    getting rid of unused serialization func

--- src/currency_core/blockchain_storage_boost_serialization.h
+++ src/currency_core/blockchain_storage_boost_serialization.h
@@ -34,15 +34,5 @@ namespace boost
       ar & ei.stake_hash;
     }
 
-    template<class archive_t>
-    void serialize(archive_t & ar, currency::extra_alias_entry_base& ai, const unsigned int version)
-    {
-      ar & ai.m_address.spend_public_key;
-      ar & ai.m_address.view_public_key;
-      ar & ai.m_view_key;
-      ar & ai.m_sign;
-      ar & ai.m_text_comment;
-      //ar & ai.;
-    }
   }
 }
