1d7bd3bda74fbdd4649b3c82c98b7ec07c7f5f1c
Nazar <45450850+NazarUsov@users.noreply.github.com>
2020-11-30 23:03:25 +0200

    all three tasks have been completed (#253)
    * all three tasks have been completed
    
    * created copy seed phrase

--- src/gui/qt-daemon/html_source/src/app/restore-wallet/restore-wallet.component.ts
+++ src/gui/qt-daemon/html_source/src/app/restore-wallet/restore-wallet.component.ts
@@ -1,42 +1,57 @@
-import {Component, NgZone, OnInit} from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import {BackendService} from '../_helpers/services/backend.service';
-import {VariablesService} from '../_helpers/services/variables.service';
-import {ModalService} from '../_helpers/services/modal.service';
-import {Wallet} from '../_helpers/models/wallet.model';
-import {TranslateService} from '@ngx-translate/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BackendService } from '../_helpers/services/backend.service';
+import { VariablesService } from '../_helpers/services/variables.service';
+import { ModalService } from '../_helpers/services/modal.service';
+import { Wallet } from '../_helpers/models/wallet.model';
+import { TranslateService } from '@ngx-translate/core';
+import { Subject } from 'rxjs/internal/Subject';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-restore-wallet',
   templateUrl: './restore-wallet.component.html',
-  styleUrls: ['./restore-wallet.component.scss']
+  styleUrls: ['./restore-wallet.component.scss'],
 })
-export class RestoreWalletComponent implements OnInit {
-
-  restoreForm = new FormGroup({
-    name: new FormControl('', [Validators.required, (g: FormControl) => {
-      for (let i = 0; i < this.variablesService.wallets.length; i++) {
-        if (g.value === this.variablesService.wallets[i].name) {
-          return {'duplicate': true};
-        }
-      }
-      return null;
-    }]),
-    key: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.pattern(this.variablesService.pattern)),
-    confirm: new FormControl('')
-  }, function (g: FormGroup) {
-    return g.get('password').value === g.get('confirm').value ? null : {'confirm_mismatch': true};
-  });
+export class RestoreWalletComponent implements OnInit, OnDestroy {
+  restoreForm = new FormGroup(
+    {
+      name: new FormControl('', [
+        Validators.required,
+        (g: FormControl) => {
+          for (let i = 0; i < this.variablesService.wallets.length; i++) {
+            if (g.value === this.variablesService.wallets[i].name) {
+              return { duplicate: true };
+            }
+          }
+          return null;
+        },
+      ]),
+      key: new FormControl('', Validators.required),
+      password: new FormControl(
+        '',
+        Validators.pattern(this.variablesService.pattern)
+      ),
+      confirm: new FormControl(''),
+      seedPassword: new FormControl('', Validators.pattern(this.variablesService.pattern)),
+    },
+    function (g: FormGroup) {
+      return g.get('password').value === g.get('confirm').value
+        ? null
+        : { confirm_mismatch: true };
+    }
+  );
 
   wallet = {
-    id: ''
+    id: '',
   };
 
   walletSaved = false;
   walletSavedName = '';
   progressWidth = '9rem';
+  seedPhraseInfo = null;
+  unsubscribeAll = new Subject<boolean>();
 
   constructor(
     private router: Router,
@@ -47,7 +62,36 @@ export class RestoreWalletComponent implements OnInit {
     private translate: TranslateService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.checkValidSeedPhrasePassword();
+    this.changeDetectionSeedPhrasePassword();
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeAll.next(true);
+    this.unsubscribeAll.complete();
+  }
+
+  changeDetectionSeedPhrasePassword() {
+    this.restoreForm.controls.seedPassword.valueChanges
+      .pipe(debounceTime(0), distinctUntilChanged(), takeUntil(this.unsubscribeAll))
+      .subscribe(() => {
+        this.checkValidSeedPhrasePassword();
+      });
+    this.restoreForm.controls.key.valueChanges
+      .pipe(debounceTime(0), distinctUntilChanged(), takeUntil(this.unsubscribeAll))
+      .subscribe(() => {
+        this.checkValidSeedPhrasePassword();
+      });
+  }
+
+  checkValidSeedPhrasePassword() {
+    const seed_password = this.restoreForm.controls.seedPassword.value;
+    const seed_phrase = this.restoreForm.controls.key.value;
+    this.backend.getSeedPhraseInfo({seed_phrase, seed_password}, (status, data) => {
+      this.seedPhraseInfo = data;
+    });
+  }
 
   createWallet() {
     this.ngZone.run(() => {
@@ -57,66 +101,133 @@ export class RestoreWalletComponent implements OnInit {
   }
 
   saveWallet() {
-    if (this.restoreForm.valid && this.restoreForm.get('name').value.length <= this.variablesService.maxWalletNameLength) {
-      this.backend.isValidRestoreWalletText(this.restoreForm.get('key').value, (valid_status, valid_data) => {
-        if (valid_data !== 'TRUE') {
-          this.ngZone.run(() => {
-            this.restoreForm.get('key').setErrors({key_not_valid: true});
-          });
-        } else {
-          this.backend.saveFileDialog(this.translate.instant('RESTORE_WALLET.CHOOSE_PATH'), '*', this.variablesService.settings.default_path, (save_status, save_data) => {
-            if (save_status) {
-              this.variablesService.settings.default_path = save_data.path.substr(0, save_data.path.lastIndexOf('/'));
-              this.walletSavedName = save_data.path.substr(save_data.path.lastIndexOf('/') + 1, save_data.path.length - 1);
-              this.backend.restoreWallet(save_data.path, this.restoreForm.get('password').value, this.restoreForm.get('key').value, (restore_status, restore_data) => {
-                if (restore_status) {
-                  this.wallet.id = restore_data.wallet_id;
-                  this.variablesService.opening_wallet = new Wallet(
-                    restore_data.wallet_id,
-                    this.restoreForm.get('name').value,
-                    this.restoreForm.get('password').value,
-                    restore_data['wi'].path,
-                    restore_data['wi'].address,
-                    restore_data['wi'].balance,
-                    restore_data['wi'].unlocked_balance,
-                    restore_data['wi'].mined_total,
-                    restore_data['wi'].tracking_hey
+    if (
+      this.restoreForm.valid &&
+      this.restoreForm.get('name').value.length <=
+        this.variablesService.maxWalletNameLength
+    ) {
+      this.backend.isValidRestoreWalletText(
+        {
+          seed_phrase: this.restoreForm.get('key').value,
+          seed_password: this.restoreForm.get('seedPassword').value,
+        },
+        (valid_status, valid_data) => {
+          if (valid_data !== 'TRUE') {
+            this.ngZone.run(() => {
+              this.restoreForm.get('key').setErrors({ key_not_valid: true });
+            });
+          } else {
+            this.backend.saveFileDialog(
+              this.translate.instant('RESTORE_WALLET.CHOOSE_PATH'),
+              '*',
+              this.variablesService.settings.default_path,
+              (save_status, save_data) => {
+                if (save_status) {
+                  this.variablesService.settings.default_path = save_data.path.substr(
+                    0,
+                    save_data.path.lastIndexOf('/')
                   );
-                  this.variablesService.opening_wallet.is_auditable = restore_data['wi'].is_auditable;
-                  this.variablesService.opening_wallet.is_watch_only = restore_data['wi'].is_watch_only;
-                  this.variablesService.opening_wallet.currentPage = 1;
-                  this.variablesService.opening_wallet.alias = this.backend.getWalletAlias(this.variablesService.opening_wallet.address);
-                  this.variablesService.opening_wallet.pages = new Array(1).fill(1);
-                  this.variablesService.opening_wallet.totalPages = 1;
-                  this.variablesService.opening_wallet.currentPage = 1;
-                  this.variablesService.opening_wallet.total_history_item = 0;
-                  this.variablesService.opening_wallet.restore = true;
-                  if (restore_data.recent_history && restore_data.recent_history.history) {
-                    this.variablesService.opening_wallet.totalPages = Math.ceil( restore_data.recent_history.total_history_items / this.variablesService.count);
-                    this.variablesService.opening_wallet.totalPages > this.variablesService.maxPages
-                      ? this.variablesService.opening_wallet.pages = new Array(5).fill(1).map((value, index) => value + index)
-                        : this.variablesService.opening_wallet.pages = new Array(this.variablesService.opening_wallet.totalPages).fill(1).map((value, index) => value + index);
-                    this.variablesService.opening_wallet.prepareHistory(restore_data.recent_history.history);
-                  }
-                  this.backend.getContracts(this.variablesService.opening_wallet.wallet_id, (contracts_status, contracts_data) => {
-                    if (contracts_status && contracts_data.hasOwnProperty('contracts')) {
-                      this.ngZone.run(() => {
-                        this.variablesService.opening_wallet.prepareContractsAfterOpen(contracts_data.contracts, this.variablesService.exp_med_ts, this.variablesService.height_app, this.variablesService.settings.viewedContracts, this.variablesService.settings.notViewedContracts);
-                      });
+                  this.walletSavedName = save_data.path.substr(
+                    save_data.path.lastIndexOf('/') + 1,
+                    save_data.path.length - 1
+                  );
+                  this.backend.restoreWallet(
+                    save_data.path,
+                    this.restoreForm.get('password').value,
+                    this.restoreForm.get('key').value,
+                    this.restoreForm.get('seedPassword').value,
+                    (restore_status, restore_data) => {
+                      if (restore_status) {
+                        this.wallet.id = restore_data.wallet_id;
+                        this.variablesService.opening_wallet = new Wallet(
+                          restore_data.wallet_id,
+                          this.restoreForm.get('name').value,
+                          this.restoreForm.get('password').value,
+                          restore_data['wi'].path,
+                          restore_data['wi'].address,
+                          restore_data['wi'].balance,
+                          restore_data['wi'].unlocked_balance,
+                          restore_data['wi'].mined_total,
+                          restore_data['wi'].tracking_hey
+                        );
+                        this.variablesService.opening_wallet.is_auditable =
+                          restore_data['wi'].is_auditable;
+                        this.variablesService.opening_wallet.is_watch_only =
+                          restore_data['wi'].is_watch_only;
+                        this.variablesService.opening_wallet.currentPage = 1;
+                        this.variablesService.opening_wallet.alias = this.backend.getWalletAlias(
+                          this.variablesService.opening_wallet.address
+                        );
+                        this.variablesService.opening_wallet.pages = new Array(
+                          1
+                        ).fill(1);
+                        this.variablesService.opening_wallet.totalPages = 1;
+                        this.variablesService.opening_wallet.currentPage = 1;
+                        this.variablesService.opening_wallet.total_history_item = 0;
+                        this.variablesService.opening_wallet.restore = true;
+                        if (
+                          restore_data.recent_history &&
+                          restore_data.recent_history.history
+                        ) {
+                          this.variablesService.opening_wallet.totalPages = Math.ceil(
+                            restore_data.recent_history.total_history_items /
+                              this.variablesService.count
+                          );
+                          this.variablesService.opening_wallet.totalPages >
+                          this.variablesService.maxPages
+                            ? (this.variablesService.opening_wallet.pages = new Array(
+                                5
+                              )
+                                .fill(1)
+                                .map((value, index) => value + index))
+                            : (this.variablesService.opening_wallet.pages = new Array(
+                                this.variablesService.opening_wallet.totalPages
+                              )
+                                .fill(1)
+                                .map((value, index) => value + index));
+                          this.variablesService.opening_wallet.prepareHistory(
+                            restore_data.recent_history.history
+                          );
+                        }
+                        this.backend.getContracts(
+                          this.variablesService.opening_wallet.wallet_id,
+                          (contracts_status, contracts_data) => {
+                            if (
+                              contracts_status &&
+                              contracts_data.hasOwnProperty('contracts')
+                            ) {
+                              this.ngZone.run(() => {
+                                this.variablesService.opening_wallet.prepareContractsAfterOpen(
+                                  contracts_data.contracts,
+                                  this.variablesService.exp_med_ts,
+                                  this.variablesService.height_app,
+                                  this.variablesService.settings
+                                    .viewedContracts,
+                                  this.variablesService.settings
+                                    .notViewedContracts
+                                );
+                              });
+                            }
+                          }
+                        );
+                        this.ngZone.run(() => {
+                          this.walletSaved = true;
+                          this.progressWidth = '50%';
+                        });
+                      } else {
+                        this.modalService.prepareModal(
+                          'error',
+                          'RESTORE_WALLET.NOT_CORRECT_FILE_OR_PASSWORD'
+                        );
+                      }
                     }
-                  });
-                  this.ngZone.run(() => {
-                    this.walletSaved = true;
-                    this.progressWidth = '50%';
-                  });
-                } else {
-                  this.modalService.prepareModal('error', 'RESTORE_WALLET.NOT_CORRECT_FILE_OR_PASSWORD');
+                  );
                 }
-              });
-            }
-          });
+              }
+            );
+          }
         }
-      });
+      );
     }
   }
 
@@ -132,7 +243,9 @@ export class RestoreWalletComponent implements OnInit {
     if (!exists) {
       this.backend.runWallet(this.wallet.id, (run_status, run_data) => {
         if (run_status) {
-          this.variablesService.wallets.push(this.variablesService.opening_wallet);
+          this.variablesService.wallets.push(
+            this.variablesService.opening_wallet
+          );
           if (this.variablesService.appPass) {
             this.backend.storeSecureAppData();
           }
@@ -145,7 +258,10 @@ export class RestoreWalletComponent implements OnInit {
       });
     } else {
       this.variablesService.opening_wallet = null;
-      this.modalService.prepareModal('error', 'OPEN_WALLET.WITH_ADDRESS_ALREADY_OPEN');
+      this.modalService.prepareModal(
+        'error',
+        'OPEN_WALLET.WITH_ADDRESS_ALREADY_OPEN'
+      );
       this.backend.closeWallet(this.wallet.id, () => {
         this.ngZone.run(() => {
           this.router.navigate(['/']);
--- src/gui/qt-daemon/html_source/src/app/wallet-details/wallet-details.component.ts
+++ src/gui/qt-daemon/html_source/src/app/wallet-details/wallet-details.component.ts
@@ -1,34 +1,63 @@
-import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
-import {BackendService} from '../_helpers/services/backend.service';
-import {VariablesService} from '../_helpers/services/variables.service';
-import {Router} from '@angular/router';
-import {Location} from '@angular/common';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { BackendService } from '../_helpers/services/backend.service';
+import { VariablesService } from '../_helpers/services/variables.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-wallet-details',
   templateUrl: './wallet-details.component.html',
-  styleUrls: ['./wallet-details.component.scss']
+  styleUrls: ['./wallet-details.component.scss'],
 })
 export class WalletDetailsComponent implements OnInit, OnDestroy {
   seedPhrase = '';
   showSeed = false;
+  copyAnimation = false;
+  copyAnimationTimeout;
 
   detailsForm = new FormGroup({
-    name: new FormControl('', [Validators.required, (g: FormControl) => {
-      for (let i = 0; i < this.variablesService.wallets.length; i++) {
-        if (g.value === this.variablesService.wallets[i].name) {
-          if (this.variablesService.wallets[i].wallet_id === this.variablesService.currentWallet.wallet_id) {
-            return {'same': true};
-          } else {
-            return {'duplicate': true};
+    name: new FormControl('', [
+      Validators.required,
+      (g: FormControl) => {
+        for (let i = 0; i < this.variablesService.wallets.length; i++) {
+          if (g.value === this.variablesService.wallets[i].name) {
+            if (
+              this.variablesService.wallets[i].wallet_id ===
+              this.variablesService.currentWallet.wallet_id
+            ) {
+              return { same: true };
+            } else {
+              return { duplicate: true };
+            }
           }
         }
-      }
-      return null;
-    }]),
-    path: new FormControl('')
+        return null;
+      },
+    ]),
+    path: new FormControl(''),
   });
+
+  seedPhraseForm = new FormGroup(
+    {
+      password: new FormControl(
+        '',
+        Validators.pattern(this.variablesService.pattern)
+      ),
+      confirmPassword: new FormControl(
+        '',
+        Validators.pattern(this.variablesService.pattern)
+      ),
+    },
+    { validators: this.checkPasswords }
+  );
+
+  checkPasswords(group: FormGroup) {
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.confirmPassword.value;
+
+    return pass === confirmPass ? null : { notSame: true };
+  }
 
   constructor(
     private router: Router,
@@ -40,55 +69,91 @@ export class WalletDetailsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.showSeed = false;
-    this.detailsForm.get('name').setValue(this.variablesService.currentWallet.name);
-    this.detailsForm.get('path').setValue(this.variablesService.currentWallet.path);
-    this.backend.getSmartWalletInfo(this.variablesService.currentWallet.wallet_id, (status, data) => {
-      if (data.hasOwnProperty('restore_key')) {
-        this.ngZone.run(() => {
-          this.seedPhrase = data['restore_key'].trim();
-        });
-      }
-    });
+    this.detailsForm
+      .get('name')
+      .setValue(this.variablesService.currentWallet.name);
+    this.detailsForm
+      .get('path')
+      .setValue(this.variablesService.currentWallet.path);
   }
 
   showSeedPhrase() {
     this.showSeed = true;
   }
 
+  onSubmitSeed() {
+    if (this.seedPhraseForm.valid) {
+      this.showSeedPhrase();
+      const wallet_id = this.variablesService.currentWallet.wallet_id;
+      const seed_password = this.seedPhraseForm.controls.password.value;
+      this.backend.getSmartWalletInfo(
+        { wallet_id, seed_password },
+        (status, data) => {
+          if (data.hasOwnProperty('seed_phrase')) {
+            this.ngZone.run(() => {
+              this.seedPhrase = data['seed_phrase'].trim();
+            });
+          }
+        }
+      );
+    }
+  }
+
   onSubmitEdit() {
     if (this.detailsForm.value) {
-      this.variablesService.currentWallet.name = this.detailsForm.get('name').value;
+      this.variablesService.currentWallet.name = this.detailsForm.get(
+        'name'
+      ).value;
       this.ngZone.run(() => {
-        this.router.navigate(['/wallet/' + this.variablesService.currentWallet.wallet_id]);
+        this.router.navigate([
+          '/wallet/' + this.variablesService.currentWallet.wallet_id,
+        ]);
       });
     }
   }
 
   closeWallet() {
-    this.backend.closeWallet(this.variablesService.currentWallet.wallet_id, () => {
-      for (let i = this.variablesService.wallets.length - 1; i >= 0; i--) {
-        if (this.variablesService.wallets[i].wallet_id === this.variablesService.currentWallet.wallet_id) {
-          this.variablesService.wallets.splice(i, 1);
+    this.backend.closeWallet(
+      this.variablesService.currentWallet.wallet_id,
+      () => {
+        for (let i = this.variablesService.wallets.length - 1; i >= 0; i--) {
+          if (
+            this.variablesService.wallets[i].wallet_id ===
+            this.variablesService.currentWallet.wallet_id
+          ) {
+            this.variablesService.wallets.splice(i, 1);
+          }
+        }
+        this.ngZone.run(() => {
+          if (this.variablesService.wallets.length) {
+            this.variablesService.currentWallet = this.variablesService.wallets[0];
+            this.router.navigate([
+              '/wallet/' + this.variablesService.currentWallet.wallet_id,
+            ]);
+          } else {
+            this.router.navigate(['/']);
+          }
+        });
+        if (this.variablesService.appPass) {
+          this.backend.storeSecureAppData();
         }
       }
-      this.ngZone.run(() => {
-        if (this.variablesService.wallets.length) {
-          this.variablesService.currentWallet = this.variablesService.wallets[0];
-          this.router.navigate(['/wallet/' + this.variablesService.currentWallet.wallet_id]);
-        } else {
-          this.router.navigate(['/']);
-        }
-      });
-      if (this.variablesService.appPass) {
-        this.backend.storeSecureAppData();
-      }
-    });
+    );
   }
 
   back() {
     this.location.back();
   }
 
-  ngOnDestroy() {}
+  copySeedPhrase() {
+    this.backend.setClipboard(this.seedPhrase);
+    this.copyAnimation = true;
+    this.copyAnimationTimeout = window.setTimeout(() => {
+      this.copyAnimation = false;
+    }, 2000);
+  }
 
+  ngOnDestroy() {
+    clearTimeout(this.copyAnimationTimeout);
+  }
 }
