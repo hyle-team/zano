7478b48a5684315f50177bfedcff01711c2126f4
cryptozoidberg <crypto.zoidberg@gmail.com>
2022-01-05 21:21:35 +0100

    fixed problem with missing pragma once

--- src/gui/qt-daemon/application/mainwindow.h
+++ src/gui/qt-daemon/application/mainwindow.h
@@ -4,6 +4,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#pragma once
 #include <QtWidgets>
 #include <QWebChannel>
 #include <boost/interprocess/ipc/message_queue.hpp>
