e113152cc52384c4f1a976b716c7e28bc0aa92fc
cryptozoidberg <crypto.zoidberg@gmail.com>
2022-01-06 16:41:25 +0100

    fixed compilation errors in url filter

--- src/gui/qt-daemon/application/urleventfilter.cpp
+++ src/gui/qt-daemon/application/urleventfilter.cpp
@@ -7,11 +7,13 @@ bool URLEventFilter::eventFilter(QObject *obj, QEvent *event)
           QFileOpenEvent *fileEvent = static_cast<QFileOpenEvent*>(event);
           if(!fileEvent->url().isEmpty())
           {
-            m_pmainwindow->handle_deeplink_click(fileEvent->url());
+            m_pmainwindow->handle_deeplink_click(fileEvent->url().toString());
             //QMessageBox msg;
             //msg.setText(fileEvent->url().toString());
             //msg.exec();
+            return true;
           }
+          return true;
       } else {
           // standard event processing
           return QObject::eventFilter(obj, event);
