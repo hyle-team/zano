50970e3d544172c0b434e03de0a2f32e27ddf2de
sowle <crypto.sowle@gmail.com>
2022-05-10 18:48:18 +0200

    removed that crazy annoying Boost warning (a bug in Boost 1.70, already fixed) "The use of BOOST_*_ENDIAN and BOOST_BYTE_ORDER is deprecated."

--- contrib/eos_portable_archive/eos/portable_iarchive.hpp
+++ contrib/eos_portable_archive/eos/portable_iarchive.hpp
@@ -88,6 +88,13 @@
 
 // basic headers
 #include <boost/version.hpp>
+
+// The following four lines disable annoying message in Boost 1.70 (The use of BOOST_*_ENDIAN and BOOST_BYTE_ORDER is deprecated.)
+#if BOOST_VERSION == 107000
+# define BOOST_PREDEF_DETAIL_ENDIAN_COMPAT_H
+# include <boost/predef/other/endian.h>
+#endif
+
 #include <boost/utility/enable_if.hpp>
 #include <boost/archive/basic_binary_iprimitive.hpp>
 #include <boost/archive/basic_binary_iarchive.hpp>
