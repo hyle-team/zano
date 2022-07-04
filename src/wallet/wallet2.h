572f3c01c6f47be27cfb42eaa2b77f73c86e751d
cryptozoidberg <crypto.zoidberg@gmail.com>
2022-07-04 15:27:41 +0200

    hidden amounts in wallet: fixed return type

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -1039,7 +1039,7 @@ private:
     uint64_t get_directly_spent_transfer_index_by_input_in_tracking_wallet(const currency::txin_to_key& intk);
     uint64_t get_directly_spent_transfer_index_by_input_in_tracking_wallet(const currency::zarcanum_input& inzk);
     bool is_in_hardfork_zone(uint64_t hardfork_index);
-    bool out_get_mixin_attr(const currency::tx_out_v& out_t);
+    uint8_t out_get_mixin_attr(const currency::tx_out_v& out_t);
     const crypto::public_key& out_get_pub_key(const currency::tx_out_v& out_t, std::list<currency::htlc_info>& htlc_info_list);
 
 
