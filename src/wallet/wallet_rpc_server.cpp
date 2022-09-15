8f15d60677c9364c23f2ad4c991f762134fa33b0
cryptozoidberg <crypto.zoidberg@gmail.com>
2022-09-15 22:05:10 +0200

    basic implementations over multi assets in wallet

--- src/wallet/wallet_rpc_server.cpp
+++ src/wallet/wallet_rpc_server.cpp
@@ -188,6 +188,21 @@ namespace tools
     {
       res.balance = m_wallet.balance();
       res.unlocked_balance = m_wallet.unlocked_balance();
+      uint64_t mined = 0;
+      std::unordered_map<crypto::hash, asset_balance_entry_base> balances;
+      m_wallet.balance(balances, mined);
+      auto it = balances.find(currency::null_hash);
+      if (it != balances.end())
+      {
+        res.balance = it->second.total;
+        res.unlocked_balance = it->second.unlocked;
+      }
+      for (auto el : balances)
+      {
+        res.balances.push_back(wallet_public::asset_balance_entry());
+        res.balances.back() = el.second;
+        res.balances.back().asset_id = el.first;
+      }
     }
     catch (std::exception& e)
     {
