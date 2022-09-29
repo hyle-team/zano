5bfbb8091d5420c8b0dc931af3c10ea741ac4548
sowle <crypto.sowle@gmail.com>
2022-09-29 21:49:57 +0200

    unit tests: amounts_tests restored, renamed

--- tests/unit_tests/amounts_tests.cpp
+++ tests/unit_tests/amounts_tests.cpp
@@ -1,9 +1,9 @@
+// Copyright (c) 2022 Zano Project
 // Copyright (c) 2012-2013 The Cryptonote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-/*
-#include "gtest/gtest.h"
 
+#include "gtest/gtest.h"
 #include "currency_core/currency_format_utils.h"
 
 using namespace currency;
@@ -122,5 +122,3 @@ TEST_neg(0__0);
 TEST_neg(0_0_);
 TEST_neg(_0_0);
 TEST_neg(0_0_0);
-
-*/
