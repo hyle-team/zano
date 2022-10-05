8c71ac83b779ea174ba8c60110f96f9dae71d46e
sowle <crypto.sowle@gmail.com>
2022-10-05 14:02:32 +0200

    zarcanum: main inequality type fixed

--- src/crypto/zarcanum.cpp
+++ src/crypto/zarcanum.cpp
@@ -21,11 +21,11 @@ namespace crypto
   }
 
   bool zarcanum_check_main_pos_inequality(const hash& kernel_hash, const scalar_t& blinding_mask, const scalar_t& secret_q,
-    const scalar_t& last_pow_block_id_hashed, const mp::uint256_t& z_l_div_z_D, uint64_t stake_amount, mp::uint256_t& lhs, mp::uint256_t& rhs)
+    const scalar_t& last_pow_block_id_hashed, const mp::uint256_t& z_l_div_z_D, uint64_t stake_amount, mp::uint256_t& lhs, mp::uint512_t& rhs)
   {
     scalar_t lhs_s = scalar_t(kernel_hash) * (blinding_mask + secret_q + last_pow_block_id_hashed); // == h * (f + q + f') mod l
     lhs = lhs_s.as_boost_mp_type<mp::uint256_t>();
-    rhs = z_l_div_z_D * stake_amount; // == floor( l / (z * D) ) * z * a
+    rhs = static_cast<mp::uint512_t>(z_l_div_z_D) * stake_amount; // == floor( l / (z * D) ) * z * a
 
     return lhs < rhs;  //  h * (f + q + f') mod l   <   floor( l / (z * D) ) * z * a
   }
--- src/crypto/zarcanum.h
+++ src/crypto/zarcanum.h
@@ -19,7 +19,7 @@ namespace crypto
   mp::uint256_t zarcanum_precalculate_z_l_div_z_D(const mp::uint128_t& pos_difficulty);
 
   bool zarcanum_check_main_pos_inequality(const hash& kernel_hash, const scalar_t& blinding_mask, const scalar_t& secret_q,
-  const scalar_t& last_pow_block_id_hashed, const mp::uint256_t& z_l_div_z_D_, uint64_t stake_amount, mp::uint256_t& lhs, mp::uint256_t& rhs);
+  const scalar_t& last_pow_block_id_hashed, const mp::uint256_t& z_l_div_z_D_, uint64_t stake_amount, mp::uint256_t& lhs, mp::uint512_t& rhs);
 
 
 } // namespace crypto
