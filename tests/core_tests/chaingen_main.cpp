2098bdc8695c9b7e5afc97bacc2ae1aa98c37d30
cryptozoidberg <crypto.zoidberg@gmail.com>
2022-10-10 20:47:41 +0200

    removed cycle for enumerating random on multiassets_basic_test

--- tests/core_tests/chaingen_main.cpp
+++ tests/core_tests/chaingen_main.cpp
@@ -1061,12 +1061,7 @@ int main(int argc, char* argv[])
     
     GENERATE_AND_PLAY(zarcanum_basic_test);
 
-    stop_on_first_fail = true;
-    for (uint64_t i = 0; i != 30; i++)
-    {
-      multiassets_basic_test::ts_starter = i;
-      GENERATE_AND_PLAY(multiassets_basic_test);
-    }
+    GENERATE_AND_PLAY(multiassets_basic_test);
   
     
 
