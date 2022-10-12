760ee3aa2881d2c7af87e654aaaa4a3a1e74048c
cryptozoidberg <crypto.zoidberg@gmail.com>
2022-10-12 13:01:16 +0200

    last warnings removed from coretests

--- tests/core_tests/hard_fork_2.cpp
+++ tests/core_tests/hard_fork_2.cpp
@@ -909,7 +909,7 @@ bool hard_fork_2_awo_wallets_basic_test<before_hf_2>::c1(currency::core& c, size
   static const std::wstring bob_wo_restored_filename(L"bob_wo_restored_wallet");
   static const std::wstring bob_non_auditable_filename(L"bob_non_auditable_wallet");
 
-  bool r = false, stub_bool = false;
+  bool r = false;
   
   CHECK_AND_ASSERT_MES(c.get_pool_transactions_count() == 0, false, "Incorrect txs count in the pool: " << c.get_pool_transactions_count());
   std::shared_ptr<tools::wallet2> alice_wlt   = init_playtime_test_wallet(events, c, ALICE_ACC_IDX);
@@ -1181,7 +1181,7 @@ bool hard_fork_2_alias_update_using_old_tx<before_hf_2>::generate(std::vector<te
 template<bool before_hf_2>
 bool hard_fork_2_alias_update_using_old_tx<before_hf_2>::c1(currency::core& c, size_t ev_index, const std::vector<test_event_entry>& events)
 {
-  bool r = false, stub_bool = false;
+  bool r = false;
 
   CHECK_AND_ASSERT_MES(c.get_pool_transactions_count() == 0, false, "Incorrect txs count in the pool: " << c.get_pool_transactions_count());
   std::shared_ptr<tools::wallet2> miner_wlt = init_playtime_test_wallet(events, c, MINER_ACC_IDX);
