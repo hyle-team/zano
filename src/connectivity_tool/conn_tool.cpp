dac549f0b610525f50b7395d9002708b0824f70a
sowle <crypto.sowle@gmail.com>
2022-12-02 18:14:19 +0100

    minor improvements and compilation fixes

--- src/connectivity_tool/conn_tool.cpp
+++ src/connectivity_tool/conn_tool.cpp
@@ -408,7 +408,7 @@ bool generate_genesis(const std::string& path_config, uint64_t premine_split_amo
   ss.str("");
   ss.clear();
   std::cout << ENDL << "PROOF PHRASE: " << gcp.proof_string << ENDL;
-  construct_miner_tx(0, 0, 0, 0, 0, destinations, bl.miner_tx, TRANSACTION_VERSION_PRE_HF4, gcp.proof_string, CURRENCY_MINER_TX_MAX_OUTS);
+  // construct_miner_tx(0, 0, 0, 0, 0, destinations, bl.miner_tx, TRANSACTION_VERSION_PRE_HF4, gcp.proof_string, CURRENCY_MINER_TX_MAX_OUTS);  <-- temporarily commented out; this overload needs to be re-implemented -- sowle 
   currency::blobdata txb = tx_to_blob(bl.miner_tx);
 
   //self validate block
