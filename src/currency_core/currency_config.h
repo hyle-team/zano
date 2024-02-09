3f246e2687e37b02df6ebe612927b6d50d73208a
cryptozoidberg <crypto.zoidberg@gmail.com>
2024-02-09 15:42:05 +0400

    restarting network

--- src/currency_core/currency_config.h
+++ src/currency_core/currency_config.h
@@ -10,7 +10,7 @@
 #ifndef TESTNET
 #define CURRENCY_FORMATION_VERSION                      84
 #else
-#define CURRENCY_FORMATION_VERSION                      94
+#define CURRENCY_FORMATION_VERSION                      95
 #endif
 
 #define CURRENCY_GENESIS_NONCE                          (CURRENCY_FORMATION_VERSION + 101011010121) //bender's nightmare
