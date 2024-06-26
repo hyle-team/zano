d4d3ea1d2376ac0443142b37f1c02a385000ddc5
sowle <crypto.sowle@gmail.com>
2024-06-26 14:56:37 +0200

    unit_tests: p2p_client_version_test_1 fixed

--- tests/unit_tests/p2p_client_version.cpp
+++ tests/unit_tests/p2p_client_version.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 Zano Project
+// Copyright (c) 2019-2024 Zano Project
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -9,35 +9,27 @@ TEST(p2p_client_version, test_1)
 {
   using namespace tools;
 
-  // good
+  // good (>= 2.x since HF4)
   
   ASSERT_TRUE(check_remote_client_version("10.101.999.28391[deadbeef31337-dirty]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.9.237[aabcd]"));
   ASSERT_TRUE(check_remote_client_version("3.0.2.7[aa00bcd]"));
-  ASSERT_TRUE(check_remote_client_version("1.4.2.7[aabcd]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.2.67[88f868c]"));
 
-  ASSERT_TRUE(check_remote_client_version("1.1.2.67[88f868c]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.2.67[26c00a8]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.2.67[26c00a8-dirty]"));
-  
-  ASSERT_TRUE(check_remote_client_version("1.1.0.65[40ba8cd]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.0.63[b0f376b]"));
-  
-  ASSERT_TRUE(check_remote_client_version("1.1.0.58[14bd668]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.0.58[9920eb7]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.0.58[e0d4ad8]"));
-  
-  ASSERT_TRUE(check_remote_client_version("1.1.0.57[b77b915]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.0.57[7dd61ae]"));
-  ASSERT_TRUE(check_remote_client_version("1.1.0.57[7dd61ae-dirty]"));
-
-  ASSERT_TRUE(check_remote_client_version("1.1.0.57"));
+  ASSERT_TRUE(check_remote_client_version("2.0.0.000[a]"));
+  ASSERT_TRUE(check_remote_client_version("2.4.2.7[aabcd]"));
+  ASSERT_TRUE(check_remote_client_version("2.99.0.67[26c00a8-dirty]"));
+  ASSERT_TRUE(check_remote_client_version("4.0.0.0[7dd61ae-dirty]"));
+  ASSERT_TRUE(check_remote_client_version("5.0.0.0[7dd61ae-dirty]"));
 
   // bad
   
   ASSERT_FALSE(check_remote_client_version(""));
   ASSERT_FALSE(check_remote_client_version(" "));
+  ASSERT_FALSE(check_remote_client_version("  "));
+  ASSERT_FALSE(check_remote_client_version("   "));
+
+  ASSERT_FALSE(check_remote_client_version("."));
+  ASSERT_FALSE(check_remote_client_version(".."));
+  ASSERT_FALSE(check_remote_client_version("..."));
 
   ASSERT_FALSE(check_remote_client_version("1.0.999"));
 
@@ -48,6 +40,9 @@ TEST(p2p_client_version, test_1)
   ASSERT_FALSE(check_remote_client_version("1.0.40[469]"));
   ASSERT_FALSE(check_remote_client_version("1.0.39[f77f0d7]"));
   ASSERT_FALSE(check_remote_client_version("1.0.38[f77f0d7-dirty]"));
-  ASSERT_FALSE(check_remote_client_version("1.0.37[7dd61ae-dirty]"));
+  ASSERT_FALSE(check_remote_client_version("1.99.37[7dd61ae-dirty]"));
   ASSERT_FALSE(check_remote_client_version("0.0.500[000]"));
+
+  ASSERT_FALSE(check_remote_client_version("a.1.9.237[aabcd]"));
+  ASSERT_FALSE(check_remote_client_version("x.0.57"));
 }
