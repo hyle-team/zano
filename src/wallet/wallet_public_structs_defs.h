270bb925f59d6d5ac0a4728901a003018fef09e7
cryptozoidberg <crypto.zoidberg@gmail.com>
2024-07-17 19:29:46 +0400

    updated documentation

--- src/wallet/wallet_public_structs_defs.h
+++ src/wallet/wallet_public_structs_defs.h
@@ -690,7 +690,7 @@ namespace wallet_public
         KV_SERIALIZE(destinations)     DOC_DSCR("List of destinations") DOC_EXMP_AUTO(1)     DOC_END
         KV_SERIALIZE(fee)              DOC_DSCR("Fee to be paid on behalf of sender's wallet(paid in native coins)") DOC_EXMP_AUTO(10000000000)     DOC_END
         KV_SERIALIZE(mixin)            DOC_DSCR("Specifies number of mixins(decoys) that would be used to create input, actual for pre-zarcanum outputs, for post-zarcanum outputs instead of this option, number that is defined by network hard rules(15+)") DOC_EXMP(15)     DOC_END
-        KV_SERIALIZE(payment_id)       DOC_DSCR("Hex-encoded payment_id, that normally used for user database by exchanges") DOC_EXMP_AUTO("1dfe5a88ff9effb3")     DOC_END
+        KV_SERIALIZE(payment_id)       DOC_DSCR("Hex-encoded payment_id, that normally used for user database by exchanges") DOC_EXMP_AUTO("")     DOC_END
         KV_SERIALIZE(comment)          DOC_DSCR("Text comment that is displayed in UI") DOC_EXMP_AUTO("Thanks for the coffe")     DOC_END
         KV_SERIALIZE(push_payer)       DOC_DSCR("Reveal information about sender of this transaction, basically add sender address to transaction in encrypted way, so only receiver can see who sent transaction") DOC_EXMP(false)     DOC_END
         KV_SERIALIZE(hide_receiver)    DOC_DSCR("This add to transaction information about remote address(destination), might be needed when the wallet restored from seed phrase and fully resynched, if this option were true, then sender won't be able to see remote address for sent transactions anymore.") DOC_EXMP(true)     DOC_END
@@ -706,9 +706,9 @@ namespace wallet_public
       uint64_t tx_size;
 
       BEGIN_KV_SERIALIZE_MAP()
-        KV_SERIALIZE(tx_hash)
+        KV_SERIALIZE(tx_hash)           DOC_DSCR("Has of the generated transaction(if succeded)") DOC_EXMP("01220e8304d46b940a86e383d55ca5887b34f158a7365bbcdd17c5a305814a93")     DOC_END
         KV_SERIALIZE(tx_unsigned_hex)
-        KV_SERIALIZE(tx_size)
+        KV_SERIALIZE(tx_size)           DOC_DSCR("Transaction size in bytes") DOC_EXMP(1234)     DOC_END
       END_KV_SERIALIZE_MAP()
     };
   };
