cf8004050ccc1fca417e199a0e887cdb099a97f8
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-10-10 18:10:25 +0500

    coretests: correct the test "asset_current_and_total_supplies_comparative_constraints" (#468)
    * Coretests: implement the test "asset_current_and_total_supplies_comparative_constraints"
    
    * Core tests: correct the test "asset_current_and_total_supplies_comparative_constraints"

--- tests/core_tests/multiassets_test.h
+++ tests/core_tests/multiassets_test.h
@@ -106,11 +106,11 @@ public:
   bool emit_asset_beta_with_incorrect_supply(currency::core& c, size_t ev_index, const std::vector<test_event_entry>& events) const;
   bool assert_asset_beta_not_emitted(currency::core& c, size_t ev_index, const std::vector<test_event_entry>& events) const;
   bool public_burn_asset_beta_with_incorrect_supply(currency::core& c, size_t ev_index, const std::vector<test_event_entry>& events) const;
-  bool assert_alice_final_balance_native_asset(currency::core& c, size_t ev_index, const std::vector<test_event_entry>& events) const;
+  bool assert_asset_gamma_registered(currency::core& c, size_t ev_index, const std::vector<test_event_entry>& events) const;
 
 private:
-  enum asset_position { alpha = 0, beta = 1 };
-  mutable std::array<currency::asset_descriptor_base, 2> m_adbs{};
-  mutable std::array<currency::asset_descriptor_operation, 2> m_ados_register{};
+  enum asset_position { alpha = 0, beta = 1, gamma = 2 };
+  mutable std::array<currency::asset_descriptor_base, 3> m_adbs{};
+  mutable std::array<currency::asset_descriptor_operation, 3> m_ados_register{};
   mutable currency::asset_descriptor_operation m_ado_emit{};
 };
