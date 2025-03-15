3a29780d8f6a3d8802a548d896d90ba63b20a6ad
cryptozoidberg <crypto.zoidberg@gmail.com>
2025-03-15 13:35:17 +0400

    added diagnostic message

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -417,6 +417,13 @@ bool simple_wallet::init(const boost::program_options::variables_map& vm)
 {
   handle_command_line(vm);
 
+  if (command_line::has_arg(arg_daemon_address) && (command_line::has_arg(arg_daemon_host) || command_line::has_arg(arg_daemon_port)))
+  {
+    fail_msg_writer() << "Please use either --daemon-address=\"host:port\" or specify both --daemon-host=\"host\" and --daemon-port=\"port\".";
+    return false;
+  }
+
+
   if (!m_daemon_address.empty() && !m_daemon_host.empty() && 0 != m_daemon_port)
   {
     fail_msg_writer() << "you can't specify daemon host or port several times";
