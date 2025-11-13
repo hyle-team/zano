639d29c415b3f1249178970cdd818e42bd70df06
Dmitry Matsiukhov <dimmarvel@mail.ru>
2025-11-13 20:40:26 +0300

    delete old http stuff

--- contrib/epee/tests/src/net/test_http_socks5.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <cstdint>
-
-namespace epee { namespace tests {
-bool run_http_socks5_http_probe(const std::string& proxy_host, uint16_t proxy_port, const std::string& url, unsigned int timeout_ms = 15000);
-}} // namespaces
