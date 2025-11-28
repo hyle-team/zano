53ae9c1de5f7759045a030c3e32da29ca10eb7aa
sowle <crypto.sowle@gmail.com>
2025-11-28 19:40:45 +0100

    test simulations

--- tests/performance_tests/aggregation_simulations.cpp
+++ tests/performance_tests/aggregation_simulations.cpp
@@ -25,7 +25,7 @@
 #include <random>
 
 namespace mp = boost::multiprecision;
-const mp::uint256_t c_scalar_L("7237005577332262213973186563042994252870990258390"); // L is the order of the main subgroup
+const mp::uint256_t c_scalar_L("7237005577332262213973186563042994240857116359379907606001950938285454250989"); // L is the order of the main subgroup
 
 static thread_local std::mt19937_64 rng(std::random_device{}());
 static thread_local std::uniform_int_distribution<uint64_t> dist_uint64;
@@ -36,13 +36,12 @@ boost::random::uniform_int_distribution<mp::uint256_t> dist_256(0, c_scalar_L - 
 
 
 
-bool is_utxo_eligibile(uint64_t amount, mp::uint256_t random, mp::uint128_t D, uint64_t epoch)
+bool is_utxo_eligibile(uint64_t amount, mp::uint256_t random, mp::uint128_t pos_difficulty, uint64_t epoch)
 {
+  if (random >= c_scalar_L)
+    throw std::runtime_error("random value must be in [0; L)");
   //////////////
-
-  const mp::uint128_t pos_difficulty = D;
-  const mp::uint256_t c_zarcanum_z_coeff_mp = mp::pow(mp::uint256_t(2), 64); // 2^64
-  
+  static const mp::uint256_t c_zarcanum_z_coeff_mp = mp::pow(mp::uint256_t(2), 64); // 2^64
   mp::uint256_t z_l_div_z_D = c_zarcanum_z_coeff_mp * (c_scalar_L / (c_zarcanum_z_coeff_mp * pos_difficulty)); // == z * floor( l / (z * D) )
   mp::uint256_t lhs = random; // use random here, originally it was scalar_t lhs_s = scalar_t(kernel_hash) * (blinding_mask + secret_q + last_pow_block_id_hashed); // == h * (f + q + f') mod l
   mp::uint512_t rhs = static_cast<mp::uint512_t>(z_l_div_z_D) * amount; // == floor( l / (z * D) ) * z * a
@@ -51,11 +50,11 @@ bool is_utxo_eligibile(uint64_t amount, mp::uint256_t random, mp::uint128_t D, u
   //return false;
 }
 
-static const uint64_t COIN = 1000000000000;
+static const uint64_t COIN = 1'000'000'000'000;
 
 struct party
 {
-  uint64_t stake_amount = 100000 * COIN;
+  uint64_t stake_amount = 100'000 * COIN;
   uint64_t blocks_staked = 0;
 };
 
@@ -69,12 +68,12 @@ uint64_t do_simulation_for_D(mp::uint128_t D)
   //std::vector<party> parties(100);
 
   //staker with 5Milion coins and others with 5Milion split between each, total 10 million coins staked
-  parties.push_back(party{ 5000000 * COIN, 0 });
+  parties.push_back(party{ 5'000'000 * COIN, 0 });
 
   //100 stakers with 50000 coins each
   for (i = 0; i != 100; i++)
   {
-    parties.push_back(party{ 50000 * COIN, 0 });
+    parties.push_back(party{ 50'000 * COIN, 0 });
   }
 
 
@@ -98,7 +97,7 @@ uint64_t do_simulation_for_D(mp::uint128_t D)
 
 int run_simulations() {
   mp::uint128_t D = COIN;
-  D *= 200000000;
+  D *= 200'000'000;
   for (size_t i = 0; i != 100; i++)
   {
     do_simulation_for_D(D);
