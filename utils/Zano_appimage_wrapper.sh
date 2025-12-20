33894b916020c3db589e9636ba5930cd10b01909
Dmitry Matsiukhov <dimmarvel@mail.ru>
2025-12-20 20:09:11 +0300

    fix app image issue

--- utils/Zano_appimage_wrapper.sh
+++ utils/Zano_appimage_wrapper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 script_dir=$( dirname "$(readlink -f "$0")" )
 
-out_dir=~/.local/share/applications
+desktop_dir=$HOME/.local/share/applications
+icon_dir=$HOME/.local/share/icons/hicolor/256x256/apps
 version="$(echo ${APPIMAGE} | rev | cut -d '-' -f1,2 | rev | sed 's/\.AppImage$//')"
-out_file_name="${out_dir}/Zano-${version}.desktop"
+out_file_name="${desktop_dir}/Zano-${version}.desktop"
 
 export QTWEBENGINE_DISABLE_SANDBOX=1
 
@@ -24,17 +25,20 @@ create_desktop_icon()
 {
     target_file_name=$1
     echo "Generating icon file: $target_file_name..."
-    rm -f "${out_dir}/Zano.png"
+    mkdir -p "${desktop_dir}"
+    mkdir -p "${icon_dir}"
     rm -f $target_file_name
-    cp -Rv "${APPDIR}/usr/share/icons/hicolor/256x256/apps/Zano.png" "${out_dir}/Zano.png"
+    rm -f "${icon_dir}/Zano.png"
+    cp -f "${APPDIR}/usr/share/icons/hicolor/256x256/apps/Zano.png" "${icon_dir}/Zano.png"
+    icon_path="${icon_dir}/Zano.png"
     echo [Desktop Entry] | tee -a $target_file_name  > /dev/null
     echo Version=1.0 | tee -a $target_file_name  > /dev/null
     echo Name=Zano | tee -a $target_file_name > /dev/null
     echo GenericName=Zano | tee -a $target_file_name  > /dev/null
     echo Comment=Privacy blockchain | tee -a $target_file_name > /dev/null
-    echo Icon=${out_dir}/Zano.png | tee -a $target_file_name > /dev/null
+    echo Icon=${icon_path} | tee -a $target_file_name > /dev/null
     echo TryExec="${APPIMAGE}" | tee -a "${target_file_name}" >/dev/null
-    echo Exec=$APPIMAGE --deeplink-params=\\\\\\\"%u\\\\\\\" | tee -a $target_file_name  > /dev/null
+    echo Exec=\"${APPIMAGE}\" --deeplink-params=%u | tee -a $target_file_name  > /dev/null
     echo Terminal=false | tee -a $target_file_name  > /dev/null
     echo Type=Application | tee -a $target_file_name  > /dev/null
     echo "Categories=Qt;Utility;" | tee -a $target_file_name  > /dev/null
@@ -45,7 +49,7 @@ create_desktop_icon()
 
 create_desktop_icon $out_file_name
 
-xdg-mime default Zano.desktop x-scheme-handler/zano
+xdg-mime default "$(basename "$out_file_name")" x-scheme-handler/zano
 xdg-desktop-menu install --novendor "${out_file_name}"
 
 call_app "$@"
