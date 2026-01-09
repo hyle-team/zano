a33f87ffe8b62ab354b8ce10cc3471d8c521b093
cryptozoidberg <crypto.zoidberg@gmail.com>
2026-01-09 23:17:07 +0400

    type-based hf terms verification table(very fancy!)

--- src/currency_core/blockchain_storage.cpp
+++ src/currency_core/blockchain_storage.cpp
@@ -6448,9 +6448,10 @@ struct visitor_proxy : public boost::static_visitor<const x_type*>
   const x_type* operator()(const t_type& v)const { return nullptr; }
 };
 
+//------------------------------------------------------------------
+  template<typename T>
+  struct type_hf_terms_traits;
 
-bool blockchain_storage::validate_tx_for_hardfork_specific_terms_types_new(const transaction& tx, const crypto::hash& tx_id, uint64_t block_height) const
-{
   enum allowance
   {
     no = 0,
@@ -6463,68 +6464,171 @@ bool blockchain_storage::validate_tx_for_hardfork_specific_terms_types_new(const
   const uint8_t extra = 0x04;
   const uint8_t attach = 0x08;
   const uint8_t signtr = 0x10;
-  const uint8_t proofs = 0x20; 
+  const uint8_t proofs = 0x20;
 
-  struct hardfork_to_type_entry 
+#define DEFINE_TERMS(hf0_val, hf1_val, hf2_val, hf3_val, hf4_val, hf5_val, hf6_val, hf7_val, container_val, Type) \
+  template <> struct type_hf_terms_traits<Type> {                     \
+    static constexpr allowance hf[] = { hf0_val, hf1_val, hf2_val, hf3_val, hf4_val, hf5_val, hf6_val, hf7_val }; \
+    static constexpr uint8_t container = container_val;   \
+  };
+    
+   
+ //hard fork id:  0     1     2     3     4     5     6     7
+ // 
+ //payloads
+  DEFINE_TERMS(   many, many, many, many, many, many, many, many,   extra|attach,      tx_service_attachment              );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  no,   no,     extra,             tx_comment                         );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  no,   no,     extra|attach,      tx_payer_old                       );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  no,   no,     extra|attach,      tx_receiver_old                    );
+  DEFINE_TERMS(   no,   no,   one,  one,  one,  one,  no,   no,     extra|attach,      tx_payer                           );
+  DEFINE_TERMS(   no,   no,   one,  one,  one,  one,  no,   no,     extra|attach,      tx_receiver                        );
+  DEFINE_TERMS(   many, many, many, many, many, many, many, many,   extra,             tx_derivation_hint                 );
+  DEFINE_TERMS(   many, many, many, many, many, many, many, many,   extra|attach,      std::string                        );
+  DEFINE_TERMS(   many, many, many, many, many, many, many, many,   extra|attach,      tx_crypto_checksum                 );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             etc_tx_time                        );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  no,   no,     extra,             etc_tx_details_unlock_time         );
+  DEFINE_TERMS(   no,   one,  one,  one,  one,  one,  no,   no,     extra,             etc_tx_details_unlock_time2        );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             etc_tx_details_expiration_time     );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             etc_tx_details_flags               );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             crypto::public_key                 );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             extra_attachment_info              );
+  DEFINE_TERMS(   one,  one,  no,   no,   no,   no,   no,   no,     extra,             extra_alias_entry_old              );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             extra_user_data                    );   //how we use this?
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             extra_padding                      );
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    extra,             etc_tx_flags16_t                   );
+  DEFINE_TERMS(   no,   no,   one,  one,  one,  one,  one,  one,    extra,             extra_alias_entry                  );
+  DEFINE_TERMS(   no,   no,   no,   no,   one,  one,  one,  one,    extra,             zarcanum_tx_data_v1                );
+  DEFINE_TERMS(   no,   no,   no,   no,   one,  one,  one,  one,    extra,             asset_descriptor_operation         );
+    //inputs
+  DEFINE_TERMS(   one,  one,  one,  one,  one,  one,  one,  one,    input,             txin_gen                           );
+  DEFINE_TERMS(   many, many, many, many, many, many, many, many,   input,             txin_to_key                        );
+  DEFINE_TERMS(   many, many, many, many, many, many, many, many,   input,             txin_multisig                      );
+  DEFINE_TERMS(   no,   no,   no,   no,   many, many, many, many,   input,             txin_zc_input                      );
+  DEFINE_TERMS(   no,   no,   no,   no,   no,   no,   many, many,   input,             txin_gateway                       );
+    //outputs
+
+  DEFINE_TERMS(   many, many, many, many, no,   no,   no,   no,     output,            tx_out_bare                        );
+  DEFINE_TERMS(   no,   no,   no,   no,   many, many, many, many,   output,            tx_out_zarcanum                    );
+  DEFINE_TERMS(   no,   no,   no,   no,   no,   no,   many, many,   output,            tx_out_gateway                     );
+    //signatures
+  DEFINE_TERMS(   many, many, many, many, many, many, many, many,   signtr,            NLSAG_sig                          );
+  DEFINE_TERMS(   no,   no,   no,   no,   many, many, many, many,   signtr,            void_sig                           );
+  DEFINE_TERMS(   no,   no,   no,   no,   many, many, many, many,   signtr,            ZC_sig                             );
+  DEFINE_TERMS(   no,   no,   no,   no,   many, many, many, many,   signtr,            zarcanum_sig                       );
+    //proofs
+  DEFINE_TERMS(   no,   no,   no,   no,   many, many, many, many,   signtr,            zc_asset_surjection_proof          );
+  DEFINE_TERMS(   no,   no,   no,   no,   many, many, many, many,   signtr,            zc_outs_range_proof                );
+  DEFINE_TERMS(   no,   no,   no,   no,   one,  one,  one,  one,    signtr,            zc_balance_proof                   );
+  DEFINE_TERMS(   no,   no,   no,   no,   one,  one,  one,  one,    signtr,            asset_operation_proof              );
+  DEFINE_TERMS(   no,   no,   no,   no,   one,  one,  one,  one,    signtr,            asset_operation_ownership_proof    );
+  DEFINE_TERMS(   no,   no,   no,   no,   one,  one,  one,  one,    signtr,            asset_operation_ownership_proof_eth);
+
+
+
+  struct proxy_visitor : public boost::static_visitor<bool>
   {
-    allowance hf0, hf1, hf2, hf3, hf4, hf5, hf6, hf7;
-    const std::type_info& tp;
-    uint8_t contains_types;
+    proxy_visitor(size_t current_hard_fork_id) :m_current_hard_fork_id(current_hard_fork_id)
+    {
+    }
+    size_t m_current_hard_fork_id;
+    uint8_t m_container_type;
+    std::map<std::type_index, size_t> m_type_occurence_count;
+
+    template<typename T>
+    bool operator()(const T& in) 
+    {
+      using t_traits = type_hf_terms_traits<T>;
+
+      //check container type
+      if (!(m_container_type & t_traits::container))
+      {
+        return false; //not allowed in this container
+      }
+
+      //check if type is allowed in this container during this hardfork
+      size_t type_count = ++m_type_occurence_count[std::type_index(typeid(T))];
+
+      CHECK_AND_ASSERT_THROW_MES(std::size(t_traits::hf) > m_current_hard_fork_id, "unexpected: m_current_hard_fork_id is bigger then hf array size");
+      switch (t_traits::hf[m_current_hard_fork_id])
+      {
+      case no:
+        return false;
+      case one:
+        if (type_count > 1)
+          return false;
+        else
+          return true;
+      case many:
+        return true;
+      default:
+        return false; //unknown type on terms map
+      }
+      return false;
+    }
   };
 
-  //TODO: Replace this with type-traits based approach, to avoid search in the array
-  const hardfork_to_type_entry tx_entries[] = { 
-  //hard fork id:    
-  //       0     1     2     3     4     5     6     7
-  //payloads
-        {  many, many, many, many, many, many, many, many,   typeid(tx_service_attachment),            extra|attach    },
-        {  one,  one,  one,  one,  one,  one,  no,   no,     typeid(tx_comment),                       extra           },
-        {  one,  one,  one,  one,  one,  one,  no,   no,     typeid(tx_payer_old),                     extra|attach    },
-        {  one,  one,  one,  one,  one,  one,  no,   no,     typeid(tx_receiver_old),                  extra|attach    },
-        {  no,   no,   one,  one,  one,  one,  no,   no,     typeid(tx_payer),                         extra|attach    },
-        {  no,   no,   one,  one,  one,  one,  no,   no,     typeid(tx_receiver),                      extra|attach    },
-        {  many, many, many, many, many, many, many, many,   typeid(tx_derivation_hint),               extra           },
-        {  many, many, many, many, many, many, many, many,   typeid(std::string),                      extra|attach    },
-        {  many, many, many, many, many, many, many, many,   typeid(tx_crypto_checksum),               extra|attach    },
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(etc_tx_time),                      extra           },
-        {  one,  one,  one,  one,  one,  one,  no,   no,     typeid(etc_tx_details_unlock_time),       extra           },
-        {  no,   one,  one,  one,  one,  one,  no,   no,     typeid(etc_tx_details_unlock_time2),      extra           },
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(etc_tx_details_expiration_time),   extra           },
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(etc_tx_details_flags),             extra           },
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(crypto::public_key),               extra           },
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(extra_attachment_info),            extra           },
-        {  one,  one,  no,   no,   no,   no,   no,   no,     typeid(extra_alias_entry_old),            extra           },
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(extra_user_data),                  extra           },   //how we use this? 
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(extra_padding),                    extra           },
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(etc_tx_flags16_t),                 extra           },
-        {  no,   no,   one,  one,  one,  one,  one,  one,    typeid(extra_alias_entry),                extra           },
-        {  no,   no,   no,   no,   one,  one,  one,  one,    typeid(zarcanum_tx_data_v1),              extra           },
-        {  no,   no,   no,   no,   one,  one,  one,  one,    typeid(asset_descriptor_operation),       extra           },
-  //inputs
-        {  one,  one,  one,  one,  one,  one,  one,  one,    typeid(txin_gen),                            input        },
-        {  many, many, many, many, many, many, many, many,   typeid(txin_to_key),                         input        },
-        {  many, many, many, many, many, many, many, many,   typeid(txin_multisig),                       input        },
-        {  no,   no,   no,   no,   many, many, many, many,   typeid(txin_zc_input),                       input        },
-        {  no,   no,   no,   no,   no,   no,   many, many,   typeid(txin_gateway),                        input        },
-  //outputs
+bool blockchain_storage::validate_tx_for_hardfork_specific_terms_types_new(const transaction& tx, const crypto::hash& tx_id, uint64_t block_height) const
+{
+  size_t current_hard_fork_id = m_core_runtime_config.hard_forks.get_the_most_recent_hardfork_id_for_height(block_height);
 
-        {  many, many, many, many, no,   no,   no,   no,     typeid(tx_out_bare),                         output       },
-        {  no,   no,   no,   no,   many, many, many, many,   typeid(tx_out_zarcanum),                     output       },
-        {  no,   no,   no,   no,   no,   no,   many, many,   typeid(tx_out_gateway),                      output       },
-  //signatures
-        {  many, many, many, many, many, many, many, many,   typeid(NLSAG_sig),                           signtr       },
-        {  no,   no,   no,   no,   many, many, many, many,   typeid(void_sig),                            signtr       },
-        {  no,   no,   no,   no,   many, many, many, many,   typeid(ZC_sig),                              signtr       },
-        {  no,   no,   no,   no,   many, many, many, many,   typeid(zarcanum_sig),                        signtr       },
-  //proofs
-        {  no,   no,   no,   no,   many, many, many, many,   typeid(zc_asset_surjection_proof),           signtr       },
-        {  no,   no,   no,   no,   many, many, many, many,   typeid(zc_outs_range_proof),                 signtr       },
-        {  no,   no,   no,   no,   one,  one,  one,  one,    typeid(zc_balance_proof),                    signtr       },
-        {  no,   no,   no,   no,   one,  one,  one,  one,    typeid(asset_operation_proof),               signtr       },
-        {  no,   no,   no,   no,   one,  one,  one,  one,    typeid(asset_operation_ownership_proof),     signtr       },
-        {  no,   no,   no,   no,   one,  one,  one,  one,    typeid(asset_operation_ownership_proof_eth), signtr       }
-  };
+  // go over every container in tx and check if its type is allowed at current hardfork version
 
+  proxy_visitor vstr(current_hard_fork_id);
+
+  vstr.m_container_type = extra;
+  for (const auto& el : tx.extra) 
+  {    
+    if (!boost::apply_visitor(vstr, el))
+    {
+      return false;
+    }
+  }
+
+  vstr.m_container_type = attach;
+  for (const auto& el : tx.attachment)
+  {
+    if (!boost::apply_visitor(vstr, el))
+    {
+      return false;
+    }
+  }
+
+  vstr.m_container_type = input;
+  for (const auto& el : tx.vin)
+  {
+    if (!boost::apply_visitor(vstr, el))
+    {
+      return false;
+    }
+  }
+
+  vstr.m_container_type = output;
+  for (const auto& el : tx.vout)
+  {
+    if (!boost::apply_visitor(vstr, el))
+    {
+      return false;
+    }
+  }
+
+
+  vstr.m_container_type = proofs;
+  for (const auto& el : tx.proofs)
+  {
+    if (!boost::apply_visitor(vstr, el))
+    {
+      return false;
+    }
+  }
+
+  vstr.m_container_type = signtr;
+  for (const auto& el : tx.signatures)
+  {
+    if (!boost::apply_visitor(vstr, el))
+    {
+      return false;
+    }
+  }
 
   return true;
 }
