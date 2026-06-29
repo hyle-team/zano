1451a170103ad3e208e9668b6f45210c2264430f
cryptozoidberg <crypto.zoidberg@gmail.com>
2026-06-29 20:31:35 +0400

    removed push_payer and hide_receiver from documentation

--- src/wallet/wallet_public_structs_defs.h
+++ src/wallet/wallet_public_structs_defs.h
@@ -932,8 +932,8 @@ namespace tools::wallet_public
         KV_SERIALIZE(mixin)            DOC_DSCR("Specifies number of mixins (decoys) that would be used to create input, actual for pre-Zarcanum outputs, for post-Zarcanum outputs instead of this option, number that is defined by network hard rules (15+)") DOC_EXMP(15)     DOC_END
         KV_SERIALIZE(payment_id)       DOC_HIDE
         KV_SERIALIZE(comment)          DOC_DSCR("Text comment that is displayed in UI") DOC_EXMP_AUTO("Thanks for the coffe")     DOC_END
-        KV_SERIALIZE(push_payer)       DOC_DSCR("[deprecated] Reveal information about sender of this transaction, basically add sender address to transaction in encrypted way, so only receiver can see who sent transaction") DOC_EXMP(false)     DOC_END
-        KV_SERIALIZE(hide_receiver)    DOC_DSCR("[deprecated] This add to transaction information about remote address(destination), might be needed when the wallet restored from seed phrase and fully resynched, if this option were true, then sender won't be able to see remote address for sent transactions anymore.") DOC_EXMP(true)     DOC_END
+        KV_SERIALIZE(push_payer)       DOC_HIDE
+        KV_SERIALIZE(hide_receiver)    DOC_HIDE
         KV_SERIALIZE(service_entries)  DOC_DSCR("Service entries that might be used by different apps that works on top of Zano network, not part of consensus") DOC_EXMP_AUTO(1)     DOC_END
         KV_SERIALIZE(service_entries_permanent) DOC_DSCR("Point to wallet that service_entries should be placed to 'extra' section of transaction(which won't be pruned after checkpoints)") DOC_EXMP_AUTO(1)     DOC_END
         KV_SERIALIZE(out_ids_to_spend) DOC_DSCR("[optional] List of output IDs that should only be used for this transfer. If empty or not present -- no restriction (default).") DOC_EXMP_AGGR({10, 15, 305}) DOC_END
