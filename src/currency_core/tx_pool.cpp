69392af0a2ccf7bcefe237a38a046ddab7205d29
sowle <crypto.sowle@gmail.com>
2026-08-07 12:51:38 +0200

    tx pool: added single tx rp checker (not used yet)

--- src/currency_core/tx_pool.cpp
+++ src/currency_core/tx_pool.cpp
@@ -1508,6 +1508,17 @@ namespace currency
 
     return true;
   }
+  //---------------------------------------------------------------------------------
+  bool tx_memory_pool::check_single_tx_range_proofs(const transaction& tx, const crypto::hash& tx_id)
+  {
+    std::vector<zc_outs_range_proofs_with_commitments> range_proofs_agregated;
+
+    CHECK_AND_ASSERT_MES(collect_rangeproofs_data_from_tx(tx, tx_id, range_proofs_agregated), false, "tx " << tx_id << ": collect_rangeproofs_data_from_tx failed");
+
+    CHECK_AND_ASSERT_MES(verify_multiple_zc_outs_range_proofs(range_proofs_agregated), false, "tx " << tx_id << " has invalid range proofs");
+
+    return true;
+  }
 }
 
 #undef LOG_DEFAULT_CHANNEL 
