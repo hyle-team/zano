4c3264813763fd1211f4c6f5e8df33a44a94ad29
Dmitry Matsiukhov <dimmarvel@mail.ru>
2026-08-18 08:57:54 +0300

    1200x700

--- src/gui/qt-daemon/application/mainwindow.cpp
+++ src/gui/qt-daemon/application/mainwindow.cpp
@@ -186,7 +186,7 @@ bool MainWindow::init_window()
   connect(m_view, SIGNAL(loadFinished(bool)), SLOT(on_load_finished(bool)));
 
   setCentralWidget(central_widget_to_be_set);
-  setMinimumSize(800, 600);
+  setMinimumSize(1200, 700);
   //this->setMouseTracking(true);
 
   m_view->page()->settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, true);
