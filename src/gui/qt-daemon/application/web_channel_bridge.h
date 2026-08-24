09429dba791c1f81de14e3e6d731ab67bb7e86c9
Dmitry Matsiukhov <dimmarvel@mail.ru>
2026-08-24 15:54:15 +0300

    fix copyright

--- src/gui/qt-daemon/application/web_channel_bridge.h
+++ src/gui/qt-daemon/application/web_channel_bridge.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2026 Zano Project
+// Copyright (c) 2026 Zano Project
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
